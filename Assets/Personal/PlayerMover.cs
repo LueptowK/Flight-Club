d651aad0661590aba779c82c6a677fb340e50ed6
LueptowK <kylelueptow@gmail.com>
2017-02-01 16:50:09 -0600

    Cleaning
    Moved air control management to its own function, removed a bunch of if
    statement redundancies, integrated dashing code a little bit better to
    avoid needless extra control statements

--- Assets/Personal/PlayerMover.cs
+++ Assets/Personal/PlayerMover.cs
@@ -10,9 +10,17 @@ public class PlayerMover : MonoBehaviour {
     Vector2 dashVel = Vector2.zero;
     int dashCounter;
     bool dashAvailable;
+    float moveSpeed = 13f;
+    float airSpeed = 0.8f;
+    float maxAirSpeed = 8f;
+    float dashMagnitude = 20f;
+    float gravity = 2f;
+    float jumpVel = 10f;
+    float dashEndMomentum = 0.65f;
+    int dashTime = 10;
 
-	// Use this for initialization
-	void Start () {
+    // Use this for initialization
+    void Start () {
         rb = GetComponent<Rigidbody2D>();
         ci = GetComponent<ControlInterpret>();
         collider = GetComponent<Collider2D>();
@@ -22,15 +30,7 @@ public class PlayerMover : MonoBehaviour {
 	
 	// Update is called once per frame
 	void FixedUpdate () {
-        float moveSpeed = 13f;
-        float airSpeed = 0.8f;
-        float maxAirSpeed = 8f;
-        float dashMagnitude = 20f;
-        float gravity = 2f;
-        float jumpVel = 10f;
-        float dashEndMomentum = 0.65f;
-        int dashTime = 10;
-
+        
         Vector2 move = ci.move;
         move.y = 0;
         move.x += 0.15f * Math.Sign(move.x);
@@ -47,48 +47,27 @@ public class PlayerMover : MonoBehaviour {
             if (desired.x < 0) { transform.localRotation = Quaternion.Euler(0, 180, 0); }
             else if (desired.x > 0) { transform.localRotation = Quaternion.Euler(0, 0, 0); }
             if (!dashAvailable) { dashAvailable = true; }
+            if (ci.Jump) { desired += new Vector2(0, jumpVel); }
         }
+        //else
+        //{
+        //    desired = AirControl(desired,move);
+        //}
         else
         {
-            desired.x = rb.velocity.x;
-            float newVel = rb.velocity.x + move.x * airSpeed;
-            //if the current x speed is lower than the desired speed, we use the new speed, UNLESS the new speed is higher than the max speed and the current speed is less than the max speed
-            //which is when we use the max speed, or the current speed is higher than the max speed, which is when we allow the speed to go down but not up.
-            desired.x = newVel;
-            if (Math.Abs(rb.velocity.x) < Math.Abs(newVel))
+            if (ci.Dash && dashAvailable && ci.move != Vector2.zero)
             {
-                if (Math.Abs(newVel) > maxAirSpeed && Math.Abs(rb.velocity.x) < maxAirSpeed)
-                {
-                    desired.x = maxAirSpeed * Mathf.Sign(newVel);
-                    print(desired.x);
-                }
-                else if (Math.Abs(newVel) > maxAirSpeed)
-                {
-                    desired.x = rb.velocity.x;
-                }
+                dashVel = ci.move.normalized * dashMagnitude;
+                dashCounter = dashTime;
+                dashAvailable = false;
+            }
+            else if (dashCounter == 0 && ci.Stall)
+            {
+                dashCounter = dashTime;
+                dashVel = Vector2.zero;
             }
         }
-
-        if (ci.Jump && grounded)
-        {
-            desired += new Vector2(0, jumpVel);
-        }
-
-        if (ci.Dash && !grounded && dashAvailable && ci.move != Vector2.zero)
-        {
-            dashVel = ci.move.normalized * dashMagnitude;
-            dashCounter = dashTime;
-            dashAvailable = false;
-        }
-
-        if (dashCounter == 0 && !grounded && ci.Stall)
-        {
-            dashCounter = dashTime;
-            dashVel = Vector2.zero;
-        }
         
-        rb.velocity = desired + new Vector2(0, rb.velocity.y - gravity*9.8f*Time.fixedDeltaTime);
-
         if (dashCounter > 0)
         {
             if (dashCounter == 2) { dashVel *= dashEndMomentum; }
@@ -96,15 +75,40 @@ public class PlayerMover : MonoBehaviour {
             dashCounter--;
             rb.velocity = dashVel;
         }
+        else
+        {
+            desired = AirControl(move);
+            rb.velocity = desired + new Vector2(0, rb.velocity.y - gravity * 9.8f * Time.fixedDeltaTime);
+        }
     }
+
     bool grounded
     {
         get
         {
             RaycastHit2D ray = Physics2D.Raycast(transform.position, -transform.up, collider.bounds.extents.y + 0.1f, 1<<8);
             return ray;
-
-
         }
+    }
+
+    Vector2 AirControl(Vector2 move)
+    {
+        Vector2 desired = new Vector2 (rb.velocity.x, 0);
+        float newVel = rb.velocity.x + move.x * airSpeed;
+        //if the current x speed is lower than the desired speed, we use the new speed, UNLESS the new speed is higher than the max speed and the current speed is less than the max speed
+        //which is when we use the max speed, or the current speed is higher than the max speed, which is when we allow the speed to go down but not up.
+        desired.x = newVel;
+        if (Math.Abs(rb.velocity.x) < Math.Abs(newVel))
+        {
+            if (Math.Abs(newVel) > maxAirSpeed && Math.Abs(rb.velocity.x) < maxAirSpeed)
+            {
+                desired.x = maxAirSpeed* Mathf.Sign(newVel);
+            }
+            else if (Math.Abs(newVel) > maxAirSpeed)
+            {
+                desired.x = rb.velocity.x;
+            }
+        }
+        return desired;
     }
 }
