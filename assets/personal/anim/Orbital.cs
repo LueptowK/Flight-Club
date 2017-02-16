c53c9d283eef6a86c1df5d3bb61f25015e8e579e
RZNewman <rudolfnewman2017@u.northwestern.edu>
2017-02-16 12:56:27 -0600

    sprite cleanup, orbitals for fun

---
+++ assets/personal/anim/Orbital.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Orbital : MonoBehaviour {
+    public float offset;
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void FixedUpdate () {
+        float pos = (Time.time%(Mathf.PI*2)*4)+offset* Mathf.PI;
+        transform.position = transform.parent.position+ (transform.right*Mathf.Sin(pos)+ transform.forward* Mathf.Cos(pos))*0.75f;
+	}
+}
