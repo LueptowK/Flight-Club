80819a6bc773463bd3ea238f8b6027de793e831e
LueptowK <kylelueptow@gmail.com>
2017-02-17 16:50:35 -0600

    Added the first iteration of a Player Select Screen
    Currently works with up to 3 controller (hardcoded but easy to scale).
    On Player Select screen, if you press A on a connected controller, it
    spawns default Keith. Next steps are connecting this to maps and giving
    Keiths colors/color select in this screen

---
+++ Assets/Personal/CreatePlayer.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using XInputDotNetPure;
+
+public class CreatePlayer : MonoBehaviour {
+    public GameObject Keith;
+    private PlayerInput[] inputs;
+    private bool[] active;
+
+    void Awake()
+    {
+        inputs = GetComponents<PlayerInput>();
+        active = new bool[3];
+    }
+
+    void FixedUpdate()
+    {
+        for (int i = 0; i < inputs.Length; i++)
+        {
+            if (inputs[i].AttackD && !active[i])
+            {
+                Instantiate(Keith);
+                Keith.GetComponent<PlayerInput>().PlayerNumber = i;
+                active[i] = true;
+            }
+        }
+    }
+}
