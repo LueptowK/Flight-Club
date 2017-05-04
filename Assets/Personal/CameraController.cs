71d8af266c1374352e538a319d69140d9b4e59e0
RZNewman <rudolfnewman2017@u.northwestern.edu>
2017-05-03 22:22:44 -0500

    new zoom camera. ZOOOOOOOM

--- Assets/Personal/CameraController.cs
+++ Assets/Personal/CameraController.cs
@@ -8,15 +8,25 @@ public class CameraController : MonoBehaviour
     private GameObject[] players;
     private int numPlayers;
     public float PanSmoothing = .9f;
-    public float Bottom = 5f;
+    public float zoomSmoothing = 0.04f;
+    public float Bottom = 6f;
     public float screenShake;
     public float minScreenShake = 2f;
     public float shakeSlowRate = 0.95f;
     public float damageToShakeRatio = 0.045f;
+
+    public float minCamZoom = 5;
+    public float maxCamSoftZoom = 8f;
+    public float camDivider = 3.8f;
+
     Vector3 actualPosition;
+    Camera cam;
+    float aspect;
     void Start()
     {
         actualPosition = transform.position;
+        cam = GetComponent<Camera>();
+        aspect = cam.aspect;
     }
 
     // Update is called once per frame
@@ -24,8 +34,8 @@ public class CameraController : MonoBehaviour
     {
         players = GameObject.FindGameObjectsWithTag("Player");
         numPlayers = players.Length;
-        Vector3 desiredCenter = FindCenter();
-        actualPosition = Vector3.Lerp(desiredCenter, actualPosition, PanSmoothing);
+        MoveCamera();
+        
         transform.position = actualPosition + new Vector3(Random.Range(-screenShake*damageToShakeRatio, screenShake*damageToShakeRatio), Random.Range(-screenShake*damageToShakeRatio, screenShake*damageToShakeRatio), 0);
         if(screenShake < minScreenShake)
         {
@@ -37,25 +47,61 @@ public class CameraController : MonoBehaviour
         }
     }
 
-    Vector3 FindCenter()
+    void MoveCamera()
     {
         Vector3 middle = Vector3.zero;
         float minX = players[0].transform.position.x;
-        float minY = players[0].transform.position.y;
+        float fallSpd = players[0].GetComponent<Rigidbody2D>().velocity.y;
+        fallSpd *= 0.15f;
+        fallSpd = Mathf.Min(fallSpd, -1);
+        float minY = players[0].transform.position.y+fallSpd;
+        
         float maxX = minX;
         float maxY = minY;
         for (int i = 1; i < numPlayers; i++)
         {
+            fallSpd = players[0].GetComponent<Rigidbody2D>().velocity.y;
+            fallSpd *= 0.1f;
+            fallSpd = Mathf.Min(fallSpd, -1);
+
             minX = Mathf.Min(players[i].transform.position.x, minX);
             maxX = Mathf.Max(players[i].transform.position.x, maxX);
-            minY = Mathf.Min(players[i].transform.position.y, minY);
+            minY = Mathf.Min(players[i].transform.position.y+fallSpd, minY);
             maxY = Mathf.Max(players[i].transform.position.y, maxY);
         }
-        for (int i = 0; i < numPlayers; i++)
+        
+        middle = new Vector3((maxX + minX)/2, (maxY + minY)/2, 0);
+
+        float wid = maxX - minX;
+        float high = maxY - minY;
+        float sizeV = high * aspect* 1.0f;
+        float sizeH = wid;
+        
+        float size = Mathf.Max(sizeH, sizeV);
+
+        // make it a little bigger than needed
+        size /= camDivider; //ratio for zoom
+        if(size < minCamZoom)
         {
-            middle = new Vector3((maxX + minX)/2, (maxY + minY)/2, 0);
+            size = minCamZoom;
         }
 
-        return new Vector3(middle.x, middle.y + Bottom, this.transform.position.z);
+        
+        Vector3 desiredCenter= new Vector3(middle.x, middle.y + Bottom, this.transform.position.z);
+        actualPosition = Vector3.Lerp(desiredCenter, actualPosition, PanSmoothing);
+        if (size > cam.orthographicSize)
+        {
+            cam.orthographicSize = size;
+        }
+        else
+        {
+            size *= 1.2f;
+            if(size> maxCamSoftZoom)
+            {
+                size = maxCamSoftZoom + (size - maxCamSoftZoom) * 0.35f;
+            }
+            cam.orthographicSize = Mathf.Lerp(cam.orthographicSize, size, zoomSmoothing);
+        }
+        
     }
 }
