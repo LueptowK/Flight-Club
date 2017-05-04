054225d33158aaaf26ff0cc032acc21e5e90174b
RZNewman <rudolfnewman2017@u.northwestern.edu>
2017-05-04 14:37:58 -0500

    Updated Camera

--- Assets/Personal/CameraController.cs
+++ Assets/Personal/CameraController.cs
@@ -18,15 +18,32 @@ public class CameraController : MonoBehaviour
     public float minCamZoom = 5;
     public float maxCamSoftZoom = 8f;
     public float camDivider = 3.8f;
+    public GameObject camBaseObj;
 
     Vector3 actualPosition;
     Camera cam;
     float aspect;
+    Transform camBase;
     void Start()
     {
         actualPosition = transform.position;
         cam = GetComponent<Camera>();
         aspect = cam.aspect;
+
+        if (camBaseObj)
+        {
+
+            camBase = camBaseObj.transform;
+        }
+    }
+
+    struct box
+    {
+        public float minX, maxX, minY, maxY;
+        
+
+        
+
     }
 
     // Update is called once per frame
@@ -50,25 +67,29 @@ public class CameraController : MonoBehaviour
     void MoveCamera()
     {
         Vector3 middle = Vector3.zero;
-        float minX = players[0].transform.position.x;
-        float fallSpd = players[0].GetComponent<Rigidbody2D>().velocity.y;
-        fallSpd *= 0.15f;
-        fallSpd = Mathf.Min(fallSpd, -1);
-        float minY = players[0].transform.position.y+fallSpd;
+
+        box b = getCamMod(players[0].GetComponent<Rigidbody2D>().velocity);
+        float minX = players[0].transform.position.x+b.minX;
+        float minY = players[0].transform.position.y+b.minY;
         
-        float maxX = minX;
-        float maxY = minY;
+        float maxX = players[0].transform.position.x + b.maxX;
+        float maxY = players[0].transform.position.y + b.maxX;
         for (int i = 1; i < numPlayers; i++)
         {
-            fallSpd = players[0].GetComponent<Rigidbody2D>().velocity.y;
-            fallSpd *= 0.1f;
-            fallSpd = Mathf.Min(fallSpd, -1);
+            b = getCamMod(players[i].GetComponent<Rigidbody2D>().velocity);
 
-            minX = Mathf.Min(players[i].transform.position.x, minX);
-            maxX = Mathf.Max(players[i].transform.position.x, maxX);
-            minY = Mathf.Min(players[i].transform.position.y+fallSpd, minY);
-            maxY = Mathf.Max(players[i].transform.position.y, maxY);
+
+            minX = Mathf.Min(players[i].transform.position.x+b.minX, minX);
+            maxX = Mathf.Max(players[i].transform.position.x+b.maxX, maxX);
+            minY = Mathf.Min(players[i].transform.position.y+b.minY, minY);
+            maxY = Mathf.Max(players[i].transform.position.y+b.minY, maxY);
         }
+
+        if (camBase)
+        {
+            minY = Mathf.Min(minY, camBase.transform.position.y);
+        }
+
         
         middle = new Vector3((maxX + minX)/2, (maxY + minY)/2, 0);
 
@@ -103,5 +124,24 @@ public class CameraController : MonoBehaviour
             cam.orthographicSize = Mathf.Lerp(cam.orthographicSize, size, zoomSmoothing);
         }
         
+    }
+
+    box getCamMod(Vector2 vel)
+    {
+        box mod = new box();
+        float min = 0.5f;
+
+        vel *= 0.15f;
+        mod.minY = Mathf.Min(vel.y, -min);
+        //mod.maxY = Mathf.Max(vel.y, min);
+        //mod.minX = Mathf.Min(vel.x, -min);
+        //mod.maxX = Mathf.Max(vel.x, min);
+
+        mod.maxY = 0;
+        mod.minX = 0;
+        mod.maxX = 0;
+    
+
+        return mod;
     }
 }
