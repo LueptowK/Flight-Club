e0b38d98e31c6cb4f99e3ffc9029888619ea65fc
RZNewman <rudolfnewman2017@u.northwestern.edu>
2017-05-25 10:01:44 -0500

    HP pickup

---
+++ assets/personal/Map/powerup/Powerup.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public abstract class Powerup : MonoBehaviour {
+    public GameObject partPre;
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+
+    void OnCollisionEnter2D(Collision2D other) {
+        if(other.gameObject.tag == "Player")
+        {
+            power(other.gameObject);
+            if (partPre)
+            {
+                Instantiate(partPre, other.transform);
+            }
+            Destroy(gameObject);
+        }
+    }
+    protected abstract void power(GameObject Player);
+}
---
+++ assets/personal/Map/powerup/RestoreHP.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class RestoreHP : Powerup {
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+    protected override void power(GameObject p)
+    {
+        p.GetComponent<PlayerHealth>().reset();
+    }
+}
