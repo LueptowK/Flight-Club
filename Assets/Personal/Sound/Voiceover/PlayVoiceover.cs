5e8277a4209aba0b85d260a7a71ebc1a72e1fbe3
KJImmy <kurtjimhoff@gmail.com>
2017-05-31 14:53:10 -0500

    Level 2 Start

---
+++ Assets/Personal/Sound/Voiceover/PlayVoiceover.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayVoiceover : MonoBehaviour {
+
+    public AudioClip soundToPlay;
+    private AudioSource audio;
+
+    // Use this for initialization
+    void Start()
+    {
+        AudioSource audio = GetComponent<AudioSource>();
+        audio.clip = soundToPlay;
+    }
+
+    void onTriggerEnter2D(Collider2D other)
+    {
+        audio.Play();
+    }
+}
