933a305a95d2b26d102c302927633f17de048794
RZNewman <rudolfnewman2017@u.northwestern.edu>
2017-05-31 23:16:54 -0500

    cam fixes2

---
+++ assets/personal/map/CameraBaseChanger.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraBaseChanger : MonoBehaviour {
+    GameObject main;
+    public GameObject baseCam;
+    public GameObject topCam;
+	// Use this for initialization
+	void Start () {
+        main =GameObject.Find("Main Camera");
+	}
+	
+	// Update is called once per frame
+	void OnTriggerEnter2D()
+    {
+        
+        main.GetComponent<CameraController>().setCamBase( baseCam?baseCam:null);
+        main.GetComponent<CameraController>().setCamTop(topCam?topCam:null);
+
+    }
+}
