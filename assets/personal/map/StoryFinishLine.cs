b9f625375e625fb4810e3d0b59d78fb1ef602a02
RZNewman <rudolfnewman2017@u.northwestern.edu>
2017-06-01 18:20:40 -0500

    script

---
+++ assets/personal/map/StoryFinishLine.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class StoryFinishLine : MonoBehaviour {
+    public StoryLevelManager manager;
+
+    void Start()
+    {
+        manager = GameObject.Find("Main Camera").GetComponent<StoryLevelManager>();
+    }
+
+    void OnTriggerEnter2D(Collider2D col)
+    {
+        manager.finish();
+    }
+}
