3a6e7fd0df043f6a19a010ca7f27f2eab284262a
RZNewman <rudolfnewman2017@u.northwestern.edu>
2017-05-31 23:16:28 -0500

    cam fixes

--- assets/personal/Map/CameraController.cs
+++ assets/personal/Map/CameraController.cs
@@ -17,13 +17,17 @@ public class CameraController : MonoBehaviour
 
     public float minCamZoom = 6;
     public float maxCamSoftZoom = 8f;
+    public float SoftZoomMult = 0.35f;
     public float camDivider = 3.8f;
+
+    public bool lookDown = false;
     public GameObject camBaseObj;
 
     Vector3 actualPosition;
     Camera cam;
     float aspect;
     Transform camBase;
+    Transform camTop;
     void Start()
     {
         actualPosition = transform.position;
@@ -38,7 +42,11 @@ public class CameraController : MonoBehaviour
     }
     public void setCamBase(GameObject cbo)
     {
-        camBase = cbo.transform;
+        camBase = cbo?cbo.transform:null;
+    }
+    public void setCamTop(GameObject cbo)
+    {
+        camTop = cbo?cbo.transform:null;
     }
     struct box
     {
@@ -71,15 +79,15 @@ public class CameraController : MonoBehaviour
     {
         Vector3 middle = Vector3.zero;
 
-        box b = getCamMod(players[0].GetComponent<Rigidbody2D>().velocity);
+        box b = getCamMod(players[0]);
         float minX = players[0].transform.position.x+b.minX;
         float minY = players[0].transform.position.y+b.minY;
         
         float maxX = players[0].transform.position.x + b.maxX;
-        float maxY = players[0].transform.position.y + b.maxX;
+        float maxY = players[0].transform.position.y + b.maxY;
         for (int i = 1; i < numPlayers; i++)
         {
-            b = getCamMod(players[i].GetComponent<Rigidbody2D>().velocity);
+            b = getCamMod(players[i]);
 
 
             minX = Mathf.Min(players[i].transform.position.x+b.minX, minX);
@@ -90,10 +98,15 @@ public class CameraController : MonoBehaviour
 
         if (camBase)
         {
-            minY = Mathf.Min(minY, camBase.transform.position.y);
+            //minY = Mathf.Min(minY, camBase.transform.position.y);
+            minY = camBase.transform.position.y;
+        }
+        if (camTop)
+        {
+            maxY = Mathf.Max(maxY, camTop.transform.position.y);
         }
 
-        
+
         middle = new Vector3((maxX + minX)/2, (maxY + minY)/2, 0);
 
         float wid = maxX - minX;
@@ -122,28 +135,54 @@ public class CameraController : MonoBehaviour
             size *= 1.2f;
             if(size> maxCamSoftZoom)
             {
-                size = maxCamSoftZoom + (size - maxCamSoftZoom) * 0.35f;
+                size = maxCamSoftZoom + (size - maxCamSoftZoom) * SoftZoomMult;
             }
             cam.orthographicSize = Mathf.Lerp(cam.orthographicSize, size, zoomSmoothing);
         }
         
     }
 
-    box getCamMod(Vector2 vel)
+    box getCamMod(GameObject player)
     {
+
+        Vector2 vel = player.GetComponent<Rigidbody2D>().velocity;
+        Vector2 inp = player.GetComponent<Interpreter>().move;
         box mod = new box();
-        float min = 0.5f;
+        float min = 0.0f;
 
         vel *= 0.10f;
-        mod.minY = Mathf.Min(vel.y, -min);
+        inp *= 10;
+
         //mod.maxY = Mathf.Max(vel.y, min);
         //mod.minX = Mathf.Min(vel.x, -min);
         //mod.maxX = Mathf.Max(vel.x, min);
 
+        //mod.minY = Mathf.Min(vel.y, -min);
+        //mod.maxY = 0;
+        //mod.minX = 0;
+        //mod.maxX = 0;
+
+        //mod.minY = Mathf.Min(inp.y, -min, vel.y);
+        //mod.maxY = Mathf.Max(inp.y, min);
+        //mod.minX = Mathf.Min(inp.x, -min);
+        //mod.maxX = Mathf.Max(inp.x, min);
+
+        
+        if (lookDown)
+        {
+            mod.minY = Mathf.Min(inp.y, -min, vel.y);
+        }
+        else
+        {
+            mod.minY = Mathf.Min(vel.y, -min);
+        }
+        
         mod.maxY = 0;
         mod.minX = 0;
         mod.maxX = 0;
-    
+
+
+
 
         return mod;
     }
