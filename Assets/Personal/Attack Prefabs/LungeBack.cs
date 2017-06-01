10881efcd854a0c63c909adc91e58988bbde9b23
LueptowK <kylelueptow@gmail.com>
2017-06-01 17:50:58 -0500

    Walt Back Ground

---
+++ Assets/Personal/Attack Prefabs/LungeBack.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class LungeBack : MovePhysics {
+    AttackActive a;
+
+    float speed = 22f;
+	// Use this for initialization
+	void Start () {
+        a = GetComponent<AttackActive>();
+    }
+
+    public override AtkMotion motion(Vector2 input)
+    {
+        AtkMotion m = new AtkMotion();
+        if (a.attacking)
+        {
+            m.use = true;
+            m.motion = -transform.right * speed;
+        }
+        else
+        {
+            m.use = false;
+            m.motion = Vector2.zero;
+        }
+
+        return m;
+    }
+
+    // Update is called once per frame
+    void Update () {
+		
+	}
+}
