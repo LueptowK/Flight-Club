0995568a67dee2af325793a930797d002c2d6c73
LueptowK <kylelueptow@gmail.com>
2017-10-12 16:13:59 -0500

    Player tags added
    Tags do not yet use entered text

---
+++ Assets/Personal/UI Prefabs/NoRotate.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class NoRotate : MonoBehaviour
+{
+    Quaternion rotation;
+
+    void Awake()
+    {
+        rotation = transform.rotation;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        transform.position = transform.parent.transform.position + new Vector3(0, 1.5f, 0);
+        transform.rotation = rotation;
+        if (gameObject.transform.parent.transform.localScale.y < 0)
+        {
+            transform.localScale = new Vector3(System.Math.Abs(transform.localScale.x), -System.Math.Abs(transform.localScale.y), transform.localScale.z);
+        }
+        else
+        {
+            transform.localScale = new Vector3(System.Math.Abs(transform.localScale.x), System.Math.Abs(transform.localScale.y), transform.localScale.z);
+        }
+    }
+}
--- assets/personal/Map/CreatePlayer.cs
+++ assets/personal/Map/CreatePlayer.cs
@@ -15,10 +15,7 @@ public class CreatePlayer : MonoBehaviour
     public GameObject portraitSlot2;
     public GameObject portraitSlot3;
     public GameObject portraitSlot4;
-    public GameObject[] keyboards;
-    public GameObject[] keyboardsUI;
     public GameObject[] nametags;
-    private string[] names = new string[4];
     private Shader shaderGUItext;
     private Shader shaderSpritesDefault;
     private playerColor[][] Colors = new playerColor[2][];
